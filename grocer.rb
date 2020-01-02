775060c910cf8873e831a45a7119fc7273c375be
legacy-gopher-9954 <fabianjohnson07@gmail.com>
2020-01-02 00:49:25 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -64,6 +64,9 @@ def apply_clearance(cart)
   # Consult README for inputs and outputs
   #
   # REMEMBER: This method **should** update cart
+  counter = 0
+  
+  
 end
 
 def checkout(cart, coupons)
