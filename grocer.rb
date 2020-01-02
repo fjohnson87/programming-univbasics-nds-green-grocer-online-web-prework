648acf4c64de4f4f19a6551e766bef19d2a1356a
legacy-gopher-9954 <fabianjohnson07@gmail.com>
2020-01-02 00:59:06 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -80,4 +80,5 @@ def checkout(cart, coupons)
   # BEFORE it begins the work of calculating the total (or else you might have
   # some irritated customers
   consolidate_cart = consolidate_cart(cart)
+  
 end
