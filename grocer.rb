9f216b2b363508ce1ea830e6b2e6b7f417d6beb2
legacy-gopher-9954 <fabianjohnson07@gmail.com>
2020-01-02 00:41:14 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -47,7 +47,7 @@ def apply_coupons(cart, coupons)
          :item => couponed_item_name,
          :price => [coupons][counter][:cost], [coupons][counter][:num]
          :count [coupons][counter][:num]
-         :clearance
+         :clearance =>
          
        }
       
